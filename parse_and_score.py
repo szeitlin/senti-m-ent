7413e51ba06857d8e11ec8acff16782c11765987
Samantha Zeitlin <samanthazeitlin@gmail.com>
2014-07-09 16:34:00 -0700

    try except keyerror helps

--- parse_and_score.py
+++ parse_and_score.py
@@ -5,6 +5,37 @@ import pprint
 import re
 import string
 
+def scores_to_dict():
+    '''
+    Parses the given tab-delimited text file, and returns a dictionary of words mapped to sentiment scores (as integers).
+
+    accused -2
+    assured 2
+    returns:
+    {accused:-2, assured:2}
+    '''
+
+    with open("AFINN-111.txt", 'r') as afinnfile:
+
+        scores = {} 
+
+        for line in afinnfile:
+                term, score  = line.split("\t")  
+                scores[term] = int(score)  
+    #print scores.items() # Print every (term, score) pair in the dictionary as one big list
+
+    #write dict to a file
+        
+    with open("scores_dict.py", 'w') as f:
+        f.seek(0)
+        scored = str(scores) #convert from dict to avoid buffer error
+        f.write(scored)
+    
+    afinnfile.close()
+    f.close()
+
+    return scores 
+
 def read_stream(droplet):
     '''
     read 'output.txt' json file, or similar. 
@@ -19,26 +50,46 @@ def read_stream(droplet):
 
 	    #print type(data)
             #pprint.pprint(data)
-            
-	    tweet = data['text']
-	    
-	    #parse into words
 
-	    words = p.split(tweet)     #should return a list 
-            print words 		
+            try:
+	        tweet = data['text']
+	        words = p.split(tweet)     #should return a list 
+		print words
+    	        continue
+
+ 	    except KeyError:
+		continue
   
-droplet = "snippet.txt"
-read_stream(droplet)
-
-
 #for hashtags could use regex with \A for start of string?
 
+def convert_and_compare(words, scores):
+    '''
+    for each word, everything ascii and lowercase
+    look up individual word(str) in the scores_dict
+    returns a list of scores (int)
 
-#try: 
+    '''
+    scorelist = []
+
     #convert to lowercase before comparing
+    for word in words:
+        word = word.encode('ascii', 'ignore') #change from unicode to be able to operate on strings
+        word = word.lower()			#get rid of capital letters
+        if word in scores.keys():
+           scorelist.append(scores[word])
+
+    print scorelist
+    return scorelist
+
+#droplet = "snippet.txt"
+#droplet = "testfile_other.txt"
+droplet = "problem_1_submission.txt"
+words = read_stream(droplet)
+#scores = scores_to_dict()	
+#scorelist = convert_and_compare(words, scores)
 
 
-#compare the words to the dictionary to get the scores
+#sum the scores
 
 
 #return the scores one line at a time
