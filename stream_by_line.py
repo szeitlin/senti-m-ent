e3f4fe1e199df985e7351a28fa0a7944d5ed7aeb
Samantha Zeitlin <samanthazeitlin@gmail.com>
2014-07-10 10:54:28 -0700

    read json file line by line using a generator

---
+++ stream_by_line.py
@@ -0,0 +1,34 @@
+__author__ = 'szeitlin'
+
+import json
+import re
+
+def read_stream(droplet, howmany):
+    '''
+    read 'output.txt' json file, line by line, using a generator.
+    argument 'howmany' = temp variable for number of lines to read
+    eventually want it to go all the way to the end of the file, I think (?) - check assignment details.
+
+    '''
+
+    p = re.compile(r'\W+')
+
+    with open(droplet, 'r') as f:
+        line = range(0, howmany)
+        for line in f:
+            data = json.loads(line)
+            yield data
+
+
+
+
+def print_data(g):
+    for x in g:
+        print x
+
+
+
+#droplet = "snippet.txt"
+droplet = "problem_1_submission.txt"
+g = read_stream(droplet, 10)
+print_data(g)
